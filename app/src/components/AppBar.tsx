290a4eb537d83dc991178ddb04262bacf30c5581
Alessandro De Pasquale <depasquale.a@tuta.io>
2023-08-05 15:01:40 +0200

    link behavior

--- app/src/components/AppBar.tsx
+++ app/src/components/AppBar.tsx
@@ -21,12 +21,12 @@ export const AppBar: FC = () => {
       <div className="navbar flex h-20 flex-row md:mb-2 shadow-lg bg-black text-neutral-content border-b border-zinc-600 bg-opacity-66">
         <div className="navbar-start align-items-center">
           <div className="hidden sm:inline w-22 h-22 md:p-2 ml-4">
-            <a href="https://argonsuite.org" target="_blank" rel="noopener noreferrer" className="text-secondary hover:text-white">
+            <Link href="https://argonsuite.org">
               <div className='flex flex-row place-content-center'>
                 <img src="/token.svg" className='h-12' />
                 <img src="/argontype.svg" className='h-6 mx-8 my-auto' />
               </div>
-            </a>
+            </Link>
           </div>
 
           <WalletMultiButtonDynamic className="btn-ghost btn-sm relative flex md:hidden text-lg " />
