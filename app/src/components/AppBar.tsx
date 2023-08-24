f614ac00ef6f831caeb0d827afbc8f49351bd6a4
Alessandro De Pasquale <depasquale.a@tuta.io>
2023-08-24 20:45:34 +0200

    bar border

--- app/src/components/AppBar.tsx
+++ app/src/components/AppBar.tsx
@@ -18,7 +18,7 @@ export const AppBar: FC = () => {
   return (
     <div>
       {/* NavBar / Header */}
-      <div className="navbar flex h-20 flex-row md:mb-2 shadow-lg bg-black text-neutral-content border-b border-zinc-600 bg-opacity-66">
+      <div className="navbar flex h-20 flex-row md:mb-2 bg-transparent text-neutral-content">
         <div className="navbar-start align-items-center">
           <div className="hidden sm:inline w-22 h-22 md:p-2 ml-4">
             <Link href="https://argonsuite.org">
