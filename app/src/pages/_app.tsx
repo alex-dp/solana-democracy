e3fee5ca9aa2d2039beac86c8ebfab5d52cb847c
Alessandro De Pasquale <depasquale.a@tuta.io>
2023-08-26 17:23:22 +0200

    image links

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -27,13 +27,13 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
         <meta name="msapplication-tap-highlight" content="no" />
         <meta name="theme-color" content="#000000" />
 
-        <link rel="apple-touch-icon" href="/icon.svg" />
+        <link rel="apple-touch-icon" href="/public/icon.svg" />
 
-        <link rel="icon" type="image/svg+xml" href="/icon.svg" />
-        <link rel="icon" type="image/png" href="/icon.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="mask-icon" href="/icon.svg" color="#8321C5" />
-        <link rel="shortcut icon" href="/icon.svg" />
+        <link rel="icon" type="image/svg+xml" href="/public/icon.svg" />
+        <link rel="icon" type="image/png" href="/public/icon.png" />
+        <link rel="manifest" href="/public/manifest.json" />
+        <link rel="mask-icon" href="/public/icon.svg" color="#8321C5" />
+        <link rel="shortcut icon" href="/public/icon.svg" />
 
         <meta property="og:type" content="Solana Dapp" />
         <meta property="og:title" content="Argon Suite" />
