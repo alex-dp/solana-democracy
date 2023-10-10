a1b3cbca427a467a2dfaf41e1a30cba59d9e0af7
Alessandro De Pasquale <depasquale.a@tuta.io>
2023-10-10 20:47:20 +0200

    reg row

--- app/src/components/petition/RegionRow.tsx
+++ app/src/components/petition/RegionRow.tsx
@@ -10,10 +10,10 @@ const RegionRow = ({
 }: RowProps) => {
 
     return (
-        <div className="">
-            <div className="capitalize font-bold">{description}</div>
+        <div className="flex flex-row gap-6 place-content-center">
+            <div className="uppercase font-bold">{description}</div>
             <Link href={"/petitions/" + code.toString()}>
-                <button className="btn btn-active btn-primary ml-6 gap-2">
+                <button className="btn btn-active btn-primary gap-2">
                     Explore
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="currentColor" viewBox="0 0 48 48"><path d="m24 40-2.1-2.15L34.25 25.5H8v-3h26.25L21.9 10.15 24 8l16 16Z" /></svg>
                 </button>
