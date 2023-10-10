f2a87c17c619686954a7f3678c5440a7c6f93041
Alessandro De Pasquale <depasquale.a@tuta.io>
2023-10-10 19:59:39 +0200

    link to civic, table sugar

--- app/src/views/petitions/index.tsx
+++ app/src/views/petitions/index.tsx
@@ -136,6 +136,15 @@ export const PetitionsView: FC = ({ }) => {
           Active regions
         </h4>
 
+        <Link href="https://www.civic.com/pricing/" target="_blank" className="mx-auto my-1">
+          <button className="btn btn-xs">
+          inquire with civic regarding gatekeeper networks
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="currentColor" viewBox="0 0 48 48">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9.25 42.7q-1.6 0-2.775-1.175Q5.3 40.35 5.3 38.75V9.25q0-1.65 1.175-2.825Q7.65 5.25 9.25 5.25h13.7v4H9.25v29.5h29.5v-13.7h4v13.7q0 1.6-1.175 2.775Q40.4 42.7 38.75 42.7Zm10.5-11.65L17 28.25l19-19H25.95v-4h16.8v16.8h-4v-10Z" />
+            </svg>
+          </button>
+        </Link>
+
         <label htmlFor="my-modal-4" className="btn btn-active border-2 border-purple-700 w-fit mx-auto my-2">
           Create new region
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 ml-2" fill="currentColor" viewBox="0 0 48 48">
