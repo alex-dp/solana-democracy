d00c9fd68b1cf9b0054fc0457cdca837590ce3aa
Alessandro De Pasquale <depasquale.a@tuta.io>
2023-10-11 17:31:08 +0200

    responsive bar

--- app/src/components/AppBar.tsx
+++ app/src/components/AppBar.tsx
@@ -22,34 +22,36 @@ export const AppBar: FC = () => {
       {/* NavBar / Header */}
       <div className="navbar flex h-20 flex-row md:mb-2 shadow-lg bg-black text-neutral-content border-b border-zinc-600 bg-opacity-66">
         <div className="navbar-start align-items-center">
-          <div className="hidden md:inline w-22 h-22 md:p-2 ml-10">
+          <div className="hidden md:inline w-22 h-22 md:p-2">
             <Link href="https://argonsuite.org">
-              <div className='flex flex-row place-content-center'>
-                <div className="mx-auto">
+              <div className='flex flex-row place-items-center gap-6'>
+                <div className="">
                   <Spline style={{ margin: -120 }} scene="https://prod.spline.design/4I6X2r7IWIX3Ep0R/scene.splinecode" />
                 </div>
-                <img src="/argontype.svg" className='h-6 mx-8 my-auto' />
+                <img src="/argontype.svg" className='h-6' />
               </div>
             </Link>
           </div>
           <div className='w-fit h-fit md:hidden'>
-            <WalletMultiButtonDynamic className="btn-ghost btn-sm relative flex text-lg " />
+            <WalletMultiButtonDynamic className="btn-ghost btn-sm relative flex text-lg" />
           </div>
         </div>
 
         {/* Nav Links */}
         {/* Wallet & Settings */}
         <div className="navbar-end">
-          <div className="hidden md:inline-flex align-items-center justify-items gap-6">
-            <div className='flex flex-row gap-6 w-fit my-auto'>
+          <div className="hidden md:inline-flex place-items-center justify-items gap-6 max-w-full">
+            <div className='flex flex-row gap-6 my-auto bg-black p-4 max-w-2/3 rounded-full overflow-auto'>
               <NavElement
                 label="Home"
                 href="/"
               />
+
               <NavElement
                 label="UBI"
                 href="/ubi"
               />
+              
               <NavElement
                 label="Petitions"
                 href="/petitions"
@@ -60,19 +62,19 @@ export const AppBar: FC = () => {
                 href="/mirrors"
               />
             </div>
-            <WalletMultiButtonDynamic className="btn-ghost btn-sm rounded-btn text-lg mr-6 " />
+            <WalletMultiButtonDynamic className="btn-ghost btn-sm rounded-btn text-lg mr-6" />
           </div>
+
           <label
             htmlFor="my-drawer"
             className="btn btn-square p-4 border-0 btn-lg bg-transparent mx-4 md:hidden">
-
-
             <div className="space-y-2.5">
               <div className={`h-0.5 w-8 bg-white`} />
               <div className={`h-0.5 w-8 bg-white`} />
               <div className={`h-0.5 w-8 bg-white`} />
             </div>
           </label>
+
         </div>
       </div>
     </div>
