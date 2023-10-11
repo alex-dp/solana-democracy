e1033d1c6383b50d3ba5cf23cb6bd89836ca23ca
Alessandro De Pasquale <depasquale.a@tuta.io>
2023-10-11 17:45:34 +0200

    footer cutoff

--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import { FC } from 'react';
 export const Footer: FC = () => {
     return (
-        <footer className="flex flex-row place-content-start fixed bottom-0 bg-transparent w-screen z-50 m-4 invisible sm:visible">
+        <footer className="flex flex-row place-content-start fixed bottom-0 bg-transparent w-screen z-50 m-4 invisible md:visible">
             <Link href="https://github.com/alex-dp/solana-democracy" target='_blank'>
                 <img alt="Static Badge" src="https://img.shields.io/badge/source_code-8321C5?style=for-the-badge&logo=github&label=github&cacheSeconds=10000000000" />
             </Link>
